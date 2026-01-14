eb482eae77944df1a93fd96e1e7257d6462dbeed
Voltor500 <sacjosue22@gmail.com>
2026-01-14 16:35:44 -0600

    Agregar soporte para múltiples idiomas: inglés, español y portugués

--- intellihome/lib/main.dart
+++ intellihome/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +10,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      // Soporte de idiomas: inglés, español, portugués
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''), // Inglés
+        Locale('es', ''), // Español
+        Locale('pt', ''), // Portugués
+      ],
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
