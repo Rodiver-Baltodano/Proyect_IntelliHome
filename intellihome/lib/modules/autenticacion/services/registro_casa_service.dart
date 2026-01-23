27bd6376f0f73a56bf7345d6ae49735e3aaeb6e4
Cris1177 <cristiandealvrz117@gmail.com>
2026-01-22 18:38:19 -0600

    Registro de la casa

---
+++ intellihome/lib/modules/autenticacion/services/registro_casa_service.dart
@@ -0,0 +1,48 @@
+import 'package:uuid/uuid.dart';
+import 'package:intellihome/modules/autenticacion/models/casa.dart';
+import 'package:intellihome/modules/autenticacion/repositories/casa_repositorio_json.dart';
+import 'package:intellihome/modules/autenticacion/validators/validaciones_casa.dart';
+
+class RegistroCasaServicio {
+  final CasaRepositorioJson repositorio;
+
+  RegistroCasaServicio({required this.repositorio});
+
+  Future<bool> registrarCasa({
+    required String nombre,
+    required double precioPorNoche,
+    required int maxPersonas,
+    required int habitaciones,
+    required String descripcion,
+    required List<String> fotos,
+    required String ubicacion,
+    required List<String> reglasUso,
+    required List<String> amenidades,
+  }) async {
+    // VALIDACIONES
+    if (!ValidacionesCasa.nombreValido(nombre)) return false;
+    if (!ValidacionesCasa.precioValido(precioPorNoche)) return false;
+    if (!ValidacionesCasa.personasValidas(maxPersonas)) return false;
+    if (!ValidacionesCasa.habitacionesValidas(habitaciones)) return false;
+    if (!ValidacionesCasa.descripcionValida(descripcion)) return false;
+    if (!ValidacionesCasa.fotosValidas(fotos)) return false;
+    if (!ValidacionesCasa.ubicacionValida(ubicacion)) return false;
+
+    final nuevaCasa = Casa(
+      id: const Uuid().v4(),
+      nombre: nombre,
+      precioPorNoche: precioPorNoche,
+      maxPersonas: maxPersonas,
+      habitaciones: habitaciones,
+      descripcion: descripcion,
+      fotos: fotos,
+      ubicacion: ubicacion,
+      reglasUso: reglasUso,
+      amenidades: amenidades,
+      fechaRegistro: DateTime.now(),
+    );
+
+    await repositorio.guardarCasa(nuevaCasa);
+    return true;
+  }
+}
